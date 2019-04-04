d9a132afbe4522e910e55455b65764c1eb39ef91
David Banks <amoebae@gmail.com>
2019-04-04 09:38:01 +0100

    tweak tweak

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,18 @@
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios';
+import {SAMPLE_QUERY} from '@/sample-query';
 
 export default Vue.extend({
     name: 'home',
     components: {},
     created() {
-        console.log("inside created hook");
+        console.log("inside created hook: %o", axios);
+
+        axios.post("http://localhost:8090/api/query/select", SAMPLE_QUERY).then(r => {
+            console.log("result happened");
+        });
     }
 });
 </script>
