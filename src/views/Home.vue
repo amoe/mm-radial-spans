680cdae53983f49b9a30f47e58add1c6f45da3cf
David Banks <amoebae@gmail.com>
2019-04-04 09:21:39 +0100

    initial io

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,10 @@ import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'home',
-  components: {},
+    name: 'home',
+    components: {},
+    created() {
+        console.log("inside created hook");
+    }
 });
 </script>
