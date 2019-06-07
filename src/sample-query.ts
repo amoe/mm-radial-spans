5cceb606d79fba12ed751745c4e1adbdca98d9db
David Banks <amoebae@gmail.com>
2019-06-07 14:55:57 +0100

    query changes for localhost

--- src/sample-query.ts
+++ src/sample-query.ts
@@ -51,8 +51,8 @@ export const SAMPLE_QUERY: any = {
     "isCached": false
 };
 
-export const SAMPLE_PROXIMITY_QUERY: any = {
-    "table": "1750-1825-sents",
+export const LIMITED_SAMPLE_PROXIMITY_QUERY: any = {
+    "table": "sampled",
     "proximity": "c",
     "target": "d",
     "distance": 3,
@@ -247,5 +247,204 @@ export const SAMPLE_PROXIMITY_QUERY: any = {
     "_TYPE": "proximity"
 };
 
-
-
+export function getUnlimitedProximityQuery(table: string): object {
+    return {
+        "table": table,
+        "proximity": "c",
+        "target": "d",
+        "distance": 3,
+        "innerLimit": 0,
+        "innerOffset": 0,
+        "outerLimit": 0,
+        "partition": {
+            "key": {
+                "namespace": "ob",
+                "name": "date",
+                "type": {
+                    "nullable": false,
+                    "type": "class",
+                    "typeParameters": [],
+                    "class": "java.time.LocalDate"
+                }
+            },
+            "function": "ROW_NUMBER",
+            "orderBy": {
+                "context": "",
+                "clauses": [
+                    {
+                        "numeric": false,
+                        "desc": false,
+                        "key": {
+                            "namespace": "ob",
+                            "name": "date",
+                            "type": {
+                                "nullable": false,
+                                "type": "class",
+                                "typeParameters": [],
+                                "class": "java.time.LocalDate"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        "scope": {
+            "key": {
+                "namespace": "ob",
+                "name": "trialAccount-id",
+                "type": {
+                    "nullable": false,
+                    "type": "class",
+                    "typeParameters": [],
+                    "class": "java.lang.String"
+                }
+            },
+            "function": "ROW_NUMBER",
+            "orderBy": {
+                "context": "",
+                "clauses": [
+                    {
+                        "numeric": false,
+                        "desc": false,
+                        "key": {
+                            "namespace": "ob",
+                            "name": "sentence-id",
+                            "type": {
+                                "nullable": false,
+                                "type": "class",
+                                "typeParameters": [],
+                                "class": "java.lang.String"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+        "literals": {
+            "a": {
+                "presenceIfTrueElseAbsence": true,
+                "key": {
+                    "namespace": "ob.date.time",
+                    "name": "classifier-am",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "uk.ac.susx.tag.method51.twitter.LabelDecision"
+                    }
+                },
+                "type": "present",
+                "args": "true"
+            },
+            "b": {
+                "presenceIfTrueElseAbsence": true,
+                "key": {
+                    "namespace": "ob.date.time",
+                    "name": "classifier-pm",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "uk.ac.susx.tag.method51.twitter.LabelDecision"
+                    }
+                },
+                "type": "present",
+                "args": "true"
+            },
+            "c": {
+                "presenceIfTrueElseAbsence": true,
+                "key": {
+                    "namespace": "ob.activity",
+                    "name": "apprehend-c",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "java.lang.Boolean"
+                    }
+                },
+                "type": "present",
+                "args": "true"
+            },
+            "d": {
+                "value": "nightdefinitive",
+                "key": {
+                    "namespace": "classify",
+                    "name": "classified-nightmention1-sentences-nightdefinitive",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "uk.ac.susx.tag.method51.twitter.LabelDecision"
+                    }
+                },
+                "type": "equals",
+                "args": "nightdefinitive"
+            },
+            "e": {
+                "presenceIfTrueElseAbsence": true,
+                "key": {
+                    "namespace": "obsphere",
+                    "name": "riverB-kw",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "java.lang.Boolean"
+                    }
+                },
+                "type": "present",
+                "args": "true"
+            },
+            "f": {
+                "presenceIfTrueElseAbsence": true,
+                "key": {
+                    "namespace": "ob.sphere",
+                    "name": "maritime",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "java.lang.Boolean"
+                    }
+                },
+                "type": "present",
+                "args": "true"
+            },
+            "g": {
+                "value": "definitelyam",
+                "key": {
+                    "namespace": "ob.date.time",
+                    "name": "classifier-am",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "uk.ac.susx.tag.method51.twitter.LabelDecision"
+                    }
+                },
+                "type": "equals",
+                "args": "definitelyam"
+            },
+            "h": {
+                "value": "definitelypm",
+                "key": {
+                    "namespace": "ob.date.time",
+                    "name": "classifier-pm",
+                    "type": {
+                        "nullable": false,
+                        "type": "class",
+                        "typeParameters": [],
+                        "class": "uk.ac.susx.tag.method51.twitter.LabelDecision"
+                    }
+                },
+                "type": "equals",
+                "args": "definitelypm"
+            }
+        },
+        "_TYPE": "proximity",
+        "orderBy": {
+        },
+        "limit": 0
+    };
+}
--- src/views/ProximityQueryHome.vue
+++ src/views/ProximityQueryHome.vue
@@ -1,11 +1,45 @@
 <template>
+<div>
+<input type="text" v-model="tableName"></input>
+<button @click="doQuery">do it</button>
+
+
+</div>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
+    import Vue from 'vue';
+import axios from 'axios';
+import {getUnlimitedProximityQuery} from '../sample-query';
 
-  export default Vue.extend({
-  });
+export default Vue.extend({
+    data() {
+        return {
+            tableName: 'sampled'
+        }
+    },
+    methods: {
+        doQuery() {
+            console.log("inside proximity query");
+            
+            const qry = getUnlimitedProximityQuery(this.tableName);
+            
+            axios.post("/api/query/proximity/cacheOnly", qry).then(r => {
+                axios.post("/api/query/proximity/query", qry).then(r => {
+                    console.log("result data was %o", r.data.length);
+
+
+                    const real = r.data.map(JSON.parse);
+                    console.log(real);
+                }).catch(e => {
+                    console.log("error happened, %o", e);
+                });
+            }).catch(e => {
+                console.log("error happened, %o", e);
+            });
+        }
+    }
+});
 </script>
 
 <style lang="less">
